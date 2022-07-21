a62fcee3e54947f48fac97c683552f0bf38f7acd
zhouqing801023 <68157926+zhouqing801023@users.noreply.github.com>
2022-07-21 11:16:51 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    if (input.lightLevel() > 100) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.clearScreen()
+    }
+})
